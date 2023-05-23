7423f64f99233c1353aba007d6b13f30d0c34796
Petey Eskola <74264355+PeteyE@users.noreply.github.com>
2023-05-23 13:31:24 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -50,4 +50,30 @@ def naive_bayes(table, evidence_row, target):
   return [neg, pos]
 
 
+def metrics(zip_list):
+  assert type(zip_list)==list, "Input must be a list"
+  assert all(isinstance(item, tuple) and len(item) == 2 for item in zip_list), "Input must be a zipped list of pairs"
+  for pair in zip_list:
+    assert (isinstance(pair, list) and len(pair) == 2), "Input must be a zipped list"
+    assert type(pair[0])==int and type(pair[1])==int, "Each value in the pair must be an int"
+    assert pair[0]>=0 and pair[1]>=0, "Each value in the pair must be >=0"
+  tn=0
+  fn=0
+  tp=0
+  fp=0
+  for pair in zip_list:
+    if pair == [0,0]:
+      tn+=1
+    elif pair == [1,1]:
+      tp+=1
+    elif pair == [1,0]:
+      fp+=1
+    elif pair == [0,1]:
+      fn+=1
+  accuracy = sum([p==a for p,a in zip_list])/len(zip_list)
+  precision = (tp/(tp+fp)) if (tp+fp)>0 else 0
+  recall = (tp/(tp+fn)) if (tp+fn)>0 else 0
+  f1 = 2*(precision*recall)/(precision+recall) if (precision+recall)>0 else 0
+  return {'Precision': precision, 'Recall': recall, 'F1': f1, 'Accuracy': accuracy}
+
 
